cf3ff04052b16dca54291582cc47fb0b595682c6
aya.bedewi@student.guc.edu.eg <aya.bedewi@student.guc.edu.eg>
2019-04-19 14:35:59 +0200

    [decode]

--- src/Stages/Decode.java
+++ src/Stages/Decode.java
@@ -32,12 +32,25 @@ public class Decode implements Runnable {
 		Opcode = instruction.substring(0, 5);
 		switch (Opcode) {
 		case ("00000"):
+			next.setRegister("R1", regFile.getFromRegsbyBits(instruction.substring(5, 10)));
+		next.setRegister("R2", regFile.getFromRegsbyBits(instruction.substring(10, 15)));
+		next.setRegister("Opcode",Opcode);
+		break;
+			
+			
 		case ("00001"):
 			next.setRegister("R1", regFile.getFromRegsbyBits(instruction.substring(5, 10)));
 			next.setRegister("R2", regFile.getFromRegsbyBits(instruction.substring(10, 15)));
 			next.setRegister("Opcode",Opcode);
 			break;
 		case ("00010"):
+			
+		next.setRegister("R1", regFile.getFromRegsbyBits(instruction.substring(5, 10)));
+		next.setRegister("R2", regFile.getFromRegsbyBits(instruction.substring(10, 15)));
+		next.setRegister("R3", regFile.getFromRegsbyBits(instruction.substring(15, 20)));
+		next.setRegister("Opcode",Opcode);
+		break;
+			
 		case ("00011"):
 			next.setRegister("R1", regFile.getFromRegsbyBits(instruction.substring(5, 10)));
 			next.setRegister("R2", regFile.getFromRegsbyBits(instruction.substring(10, 15)));
@@ -45,20 +58,54 @@ public class Decode implements Runnable {
 			next.setRegister("Opcode",Opcode);
 			break;
 		case ("00100"):
-		case ("00101"):
-		case ("00110"):
+			
 			next.setRegister("R1", regFile.getFromRegsbyBits(instruction.substring(5, 10)));
-			next.setRegister("Immediate", regFile.getFromRegsbyBits(instruction.substring(10)));
-			next.setRegister("Opcode",Opcode);
+		next.setRegister("immediate", regFile.getFromRegsbyBits(instruction.substring(10)));
+		next.setRegister("Opcode",Opcode);
+		break;
+			
+		case ("00101"):
+			
+
+		next.setRegister("R1", regFile.getFromRegsbyBits(instruction.substring(5, 10)));
+		next.setRegister("immediate", regFile.getFromRegsbyBits(instruction.substring(10)));
+		next.setRegister("Opcode",Opcode);
+		break;
+			
+			
+		case ("00110"):
+		next.setRegister("R1", regFile.getFromRegsbyBits(instruction.substring(5, 10)));			next.setRegister("Immediate", regFile.getFromRegsbyBits(instruction.substring(10)));
+		next.setRegister("immediate", regFile.getFromRegsbyBits(instruction.substring(10)));
+		next.setRegister("Opcode",Opcode);
+		break;
+			
 		case ("00111"):
-		case ("01000"):
-		case ("01001"):
-		case ("01010"):
+	
 			next.setRegister("R1", regFile.getFromRegsbyBits(instruction.substring(5, 10)));
 			next.setRegister("R2", regFile.getFromRegsbyBits(instruction.substring(10, 15)));
 			next.setRegister("R3", regFile.getFromRegsbyBits(instruction.substring(15, 20)));
 			next.setRegister("Opcode",Opcode);
 			break;
+		
+		case ("01000"):
+			next.setRegister("R1", regFile.getFromRegsbyBits(instruction.substring(5, 10)));
+		next.setRegister("R2", regFile.getFromRegsbyBits(instruction.substring(10, 15)));
+		next.setRegister("R3", regFile.getFromRegsbyBits(instruction.substring(15, 20)));
+		next.setRegister("Opcode",Opcode);
+		break;
+		case ("01001"):
+			next.setRegister("R1", regFile.getFromRegsbyBits(instruction.substring(5, 10)));
+		next.setRegister("R2", regFile.getFromRegsbyBits(instruction.substring(10, 15)));
+		next.setRegister("R3", regFile.getFromRegsbyBits(instruction.substring(15, 20)));
+		next.setRegister("Opcode",Opcode);
+		break;
+		case ("01010"):
+			
+			next.setRegister("R1", regFile.getFromRegsbyBits(instruction.substring(5, 10)));
+		next.setRegister("R2", regFile.getFromRegsbyBits(instruction.substring(10, 15)));
+		next.setRegister("R3", regFile.getFromRegsbyBits(instruction.substring(15, 20)));
+		next.setRegister("Opcode",Opcode);
+		break;
 		case ("01100"):
 			next.setRegister("R1", regFile.getFromRegsbyBits(instruction.substring(5, 10)));
 			next.setRegister("R2", regFile.getFromRegsbyBits(instruction.substring(10, 15)));
@@ -74,6 +121,11 @@ public class Decode implements Runnable {
 			next.setRegister("R1", regFile.getFromRegsbyBits(instruction.substring(5, 10)));
 			next.setRegister("Opcode",Opcode);
 			break;
+		case ("01111"):
+			
+			next.setRegister("Opcode",Opcode);
+		break;
+			
 		case ("10000"):
 			next.setRegister("R1", regFile.getFromRegsbyBits(instruction.substring(5, 10)));
 			next.setRegister("R2", regFile.getFromRegsbyBits(instruction.substring(10, 15)));
@@ -90,19 +142,19 @@ public class Decode implements Runnable {
 			next.setRegister("Opcode",Opcode);
 			break;
 		case ("10011"):
-			next.setRegister("rsd", regFile.getFromRegsbyBits(instruction.substring(5, 10)));
+			next.setRegister("dst", regFile.getFromRegsbyBits(instruction.substring(5)));
 			next.setRegister("Opcode",Opcode);
 			break;
 		case ("10100"):
 			next.setRegister("R1", regFile.getFromRegsbyBits(instruction.substring(5, 10)));
 			next.setRegister("R2", regFile.getFromRegsbyBits(instruction.substring(10, 15)));
-			next.setRegister("R3", regFile.getFromRegsbyBits(instruction.substring(15)));
+			next.setRegister("dst", regFile.getFromRegsbyBits(instruction.substring(15)));
 			next.setRegister("Opcode",Opcode);
 			break;
 		case ("10101"):
 			next.setRegister("R1", regFile.getFromRegsbyBits(instruction.substring(5, 10)));
 			next.setRegister("R2", regFile.getFromRegsbyBits(instruction.substring(10, 15)));
-			next.setRegister("R3", regFile.getFromRegsbyBits(instruction.substring(15)));
+			next.setRegister("dst", regFile.getFromRegsbyBits(instruction.substring(15)));
 			next.setRegister("Opcode",Opcode);
 			break;
 		}
